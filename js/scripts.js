074767f434b7b652943c9aa70f8b764d21e8f47f
m-kid <maciej.kidawa2@gmail.com>
2016-11-05 10:31:07 +0100

    nieudana próba dodania ceny przedłużonej garancji

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,26 @@ function Telefon (marka, cena, kolor) {
     this.kolor = kolor;
 }
 
+
+
 Telefon.prototype.printInfo = function() {
-    console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ".");
+
+    var self = this.cena
+    function getWarrantyCost () {
+        var warranty = 0.1 * self;
+        console.log(warranty);
+        return
+    };
+getWarrantyCost();
+
+var wynikWarranty = getWarrantyCost();
+
+    console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ". Cena przedłużonej gwarancji wynosi " + wynikWarranty + ".");
 };
 
-var Samsung = new Telefon("Samsung", "tysiondz", "moro");
 
-Samsung.printInfo();
+var samsung = new Telefon("Samsung", "1000", "moro");
+var nokia = new Telefon("Nokia", "2000","srebro");
+
+samsung.printInfo();
+nokia.printInfo();
